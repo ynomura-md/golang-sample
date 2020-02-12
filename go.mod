ebe1fbdc75b869d3c9f769ca4d23fcae650e38cd
y-nomura <yoshihiro.nomura@supership.jp>
2020-02-12 11:13:21 +0900

    ログ出力検証

--- go.mod
+++ go.mod
@@ -7,11 +7,13 @@ require (
 	github.com/gin-contrib/zap v0.0.0-20191128031730-d12829f8f61b // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	go.uber.org/atomic v1.5.1 // indirect
 	go.uber.org/multierr v1.4.0 // indirect
 	go.uber.org/zap v1.13.0
--- handler/main.go
+++ handler/main.go
@@ -6,15 +6,50 @@ import (
     "github.com/gin-contrib/zap"
     "deliver-endpoint/api"
     "go.uber.org/zap"
+    "github.com/satori/go.uuid"
+    "go.uber.org/zap/zapcore"
 )
 
-var logger *zap.Logger
+//var logger *zap.Logger
+var sugarLogger *zap.SugaredLogger
 
+type AccessLog struct {
+  Id string
+  Time time.Time
+  IP string
+  UA string
+  UID string
+  UID_TYPE string
+  PLATFORM string
+  AD_UNIT_GROUP_ID int
+  AD_UNIT_ID int
+  EXT map[string]string
+
+}
+func (a AccessLog) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+    enc.AddString("id", a.Id)
+    enc.AddString("time", a.Time.UTC().Format("2006-01-02T15:04:05.999Z"))
+    enc.AddString("ip", a.IP)
+    enc.AddString("ua", a.UA)
+    enc.AddString("uid", a.UID)
+    enc.AddString("ut", a.UID_TYPE)
+    enc.AddString("pl", a.PLATFORM)
+    enc.AddInt("augi", a.AD_UNIT_GROUP_ID)
+    enc.AddInt("aui", a.AD_UNIT_ID)
+    enc.OpenNamespace("ext")
+    for key, value := range a.EXT {
+      enc.AddString(key, value)
+    }
+    return nil
+}
 
 func main() {
-    logger, _ = zap.NewDevelopment()
+    logger, _ := zap.NewProduction()
+    sugarLogger = logger.Sugar()
+    defer sugarLogger.Sync()
     r := gin.Default()
     r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+    r.Use(sampleMiddleware())
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
@@ -24,12 +59,61 @@ func main() {
     r.Run(":9999")
 }
 
+func sampleMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        sugarLogger.Infow("before logic")
+        if uid := c.Query("uid"); len(uid) > 0 {
+          c.Set("uid", uid)
+          c.Set("uidType", "wowma") // クエリに混ぜる？
+        } else {
+          id := uuid.NewV1()
+          a,_ := id.MarshalText()
+          c.Set("uid", string(a))
+          c.Set("uidType", "tpo")
+        }
+
+        if pl := c.Query("pl"); len(pl) > 0 {
+          c.Set("pl", pl)
+        } else {
+          c.Set("pl", "unknown")
+        }
+
+        c.Next()
+        sugarLogger.Infow("after logic")
+    }
+}
+
+type AdReq struct {
+  AD_UNIT_GROUP_ID int `form:"gi"`
+  AD_UNIT_ID int `form:"ui"`
+}
+
 
 func handler(ctx *gin.Context) {
 
+    var req AdReq
+
+    ctx.Bind(&req)
+
     user := api.User{"User", 20}
 
-     logger.Info("object sample", zap.Object("userObj", user))
+    id := uuid.NewV1()
+    a,_ := id.MarshalText()
+
+    logdata := AccessLog{
+      string(a),
+      time.Now(),
+      ctx.ClientIP(),
+      ctx.GetHeader("user-agent"),
+      ctx.GetString("uid"),
+      ctx.GetString("uidType"),
+      ctx.GetString("pl"),
+      req.AD_UNIT_GROUP_ID,
+      req.AD_UNIT_ID,
+      map[string]string{"apple": "a", "banana": "1", "lemon": "1"},
+    }
+
+    sugarLogger.Infow("record", zap.Object("access",logdata))
 
 
     ctx.JSON(200, gin.H{
